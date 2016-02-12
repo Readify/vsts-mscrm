b13ed208b8d86d8650e7c4b15a33c7b2101c4e5a
Mitch Denny <mitch@mitchdenny.com>
2016-02-12 14:23:57 +1100

    Task stamping working.

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var exec = require('child_process').exec;
 var minimist = require('minimist');
 var semver = require('semver');
+var fs = require('fs');
 
 // Default version data. Should be replaced as soon as the get-version task runs.
 var version = {
@@ -28,23 +29,49 @@ gulp.task('get-version', function (callback) {
     }
 });
 
+function stampTaskJson(path) {
+    var taskDefinitionContents = fs.readFileSync(path, 'utf8');
+	var taskDefinition = JSON.parse(taskDefinitionContents);
+
+	taskDefinition.version = {
+        "Major": version.Major,
+        "Minor": version.Minor,
+        "Patch": version.Patch
+    };
+
+	taskDefinitionContents = JSON.stringify(taskDefinition, null, '\t');
+	fs.writeFileSync(path, taskDefinitionContents);    
+}
+
 gulp.task('stamp-manifest', ['get-version'], function (callback) {
+    var manifestContents = fs.readFileSync('src\\vss-extension.json', 'utf8');
+	var manifest = JSON.parse(manifestContents);
+
+	manifest.version = version.MajorMinorPatch;
+
+	manifestContents = JSON.stringify(manifest, null, '\t');
+	fs.writeFileSync('src\\vss-extension.json', manifestContents);
+    
     callback();
 });
 
 gulp.task('stamp-export-solution-task', ['get-version'], function (callback) {
+    stampTaskJson('src\\tasks\\vsts-mscrm-export-solution\\task.json');
     callback(); 
 });
 
 gulp.task('stamp-import-solution-task', ['get-version'], function (callback) {
+    stampTaskJson('src\\tasks\\vsts-mscrm-import-solution\\task.json');
     callback(); 
 });
 
 gulp.task('stamp-pack-solution-task', ['get-version'], function (callback) {
+    stampTaskJson('src\\tasks\\vsts-mscrm-pack-solution\\task.json');
     callback(); 
 });
 
 gulp.task('stamp-unpack-solution-task', ['get-version'], function (callback) {
+    stampTaskJson('src\\tasks\\vsts-mscrm-unpack-solution\\task.json');
     callback(); 
 });
 
