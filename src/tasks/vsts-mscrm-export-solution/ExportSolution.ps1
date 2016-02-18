eae96905ba3a0bf00ee8f6fe02b337446a89b8e3
Mitch Denny <mitch@mitchdenny.com>
2016-02-18 16:33:19 +1100

    Working on connected services.

--- src/tasks/vsts-mscrm-export-solution/ExportSolution.ps1
+++ src/tasks/vsts-mscrm-export-solution/ExportSolution.ps1
@@ -15,9 +15,17 @@ Import-Module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
 Write-Host "connectedServiceName=$connectedServiceName"
 Write-Host "zipFile=$zipFile"
 
+Write-Output "Getting service endpoint..."
 $serviceEndpoint = Get-ServiceEndpoint -Context $distributedTaskContext -Name $connectedServiceName
-$url = serviceEndpoint.Url
+
+$url = $serviceEndpoint.Url
 $username = $serviceEndpoint.Authorization.Parameters.UserName
 $password = $serviceEndpoint.Authorization.Parameters.Password
 
-#Import-Module .\tools\Microsoft.Xrm.Data.PowerShell\Microsoft.Xrm.Data.PowerShell.psm1
+$securePassword = ConvertTo-SecureString $password -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential -Args $username, $securePassword
+
+Write-Output "Importing PowerShell Module..."
+Import-Module .\tools\Microsoft.Xrm.Data.PowerShell\Microsoft.Xrm.Data.PowerShell.psm1
+$connection = Connect-CrmOnline -ServerUrl $url -Credential $credential
+$connection | Format-List
