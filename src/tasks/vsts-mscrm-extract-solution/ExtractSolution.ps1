02c88bfc97e3f70473718db0ae9098e8949f5de5
Matt Davies <matt.davies@readify.net>
2016-03-17 15:11:22 +0800

    Fix incorrect xml pokage

--- src/tasks/vsts-mscrm-extract-solution/ExtractSolution.ps1
+++ src/tasks/vsts-mscrm-extract-solution/ExtractSolution.ps1
@@ -13,10 +13,6 @@ $ErrorActionPreference = "Stop"
 $taskMetadata = ConvertFrom-Json -InputObject $(Get-Content $PSScriptRoot\task.json -raw)
 Write-Host "Task version is: $($taskMetadata.version)"
 
-# Import the Task.Common and Task.Internal dll that has all the cmdlets we need for Build
-import-module "Microsoft.TeamFoundation.DistributedTask.Task.Internal"
-import-module "Microsoft.TeamFoundation.DistributedTask.Task.Common"
-
 Write-Host "folder is: $folder"
 Write-Host "zipFile is: $zipFile"
 
@@ -25,5 +21,5 @@ Write-Host "zipFile is: $zipFile"
 $solutionXmlPath = "$folder\Other\Solution.xml"
 [xml] $solutionXml = Get-Content $solutionXmlPath
 $node = $solutionXml.SelectSingleNode("ImportExportXml/SolutionManifest/Version")
-$node.Value = "2"
+$node.'#text' = "2"
 $solutionXml.Save($solutionXmlPath)
